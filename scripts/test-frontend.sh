878d1ed88cb5ef76b2f5165c61d6002fe2fe40f5
martinlykke <martin@wirksomhed.dk>
2021-08-22 14:21:06 +0000

    xd

--- scripts/test-frontend.sh
+++ scripts/test-frontend.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 address=127.0.0.1:8080
+echo 'Running frontend test'
 if [ ! -z "${docker_image}" ] && [ ! -z "${docker_run_id}" ]; then
     for (( c=1; c<=5; c++ ))
     do
-        echo 'Test script is running..'
+        echo 'Docker image exists'
     done
 
 fi
