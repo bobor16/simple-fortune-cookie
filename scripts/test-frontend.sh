f5fb6ada7954f123df6a7103762a5b7d0a80b69d
martinlykke <martin@wirksomhed.dk>
2021-08-22 12:23:02 +0000

    Add test step & test script

---
+++ scripts/test-frontend.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+address=127.0.0.1:8080
+if [ ! -z "${docker_image}" ] && [ ! -z "${docker_run_id}" ]; then
+    for (( c=1; c<=5; c++ ))
+    do
+        echo 'Test script is running..'
+    done
+
+fi
