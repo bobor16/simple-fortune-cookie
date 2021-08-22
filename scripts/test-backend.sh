1354a5392c0a3ecf57e15ba1c60bb1c65fbd15d6
martinlykke <martin@wirksomhed.dk>
2021-08-22 15:06:24 +0000

    update tests

--- scripts/test-backend.sh
+++ scripts/test-backend.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 address=127.0.0.1:9000
 echo 'Running frontend test..'
-for (( c=1; c<=10; c++ ))
+for i in {1..10}
 do
     echo 'Attempting to connect..'
 
-    if [ ! -z "$(curl --silent ${address} | grep '<title>Simple Fortune Cookie</title>')" ]; then
+    if [ ! -z "$(curl ${address}/fortunes)" ]; then
         echo 'Response ok'
         exit 0
     fi
 
-    sleep 3
+    sleep 2
 done
 
 echo 'Failed to get response'
